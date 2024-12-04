a98acbfcbfa31c524da6b25850c5d875837083f8
Sachin100591 <sachin.singh@kulu.com>
2024-12-04 19:52:04 +0530

    Removed DB dependencies from all the environments
    Updated Sidekiq configs to support Redis's username and password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ require 'sidekiq/throttled'
 require 'sidekiq/throttled/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: "redis://#{ENV['REDIS_USERNAME']}:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_DB_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB_INDEX']}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: "redis://#{ENV['REDIS_USERNAME']}:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_DB_HOST']}:#{ENV['REDIS_PORT']}/#{ENV['REDIS_DB_INDEX']}" }
 end
