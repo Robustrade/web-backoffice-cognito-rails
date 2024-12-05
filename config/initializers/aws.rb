9e790152585acfec628fec71c5b849522dc7dddc
Sachin100591 <sachin.singh@kulu.com>
2024-12-05 19:59:21 +0530

    Updated the AWS intializer to handle role based access

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,27 @@
-Aws.config.update({
-  region: ENV['AWS_REGION'],
-  credentials: Aws::Credentials.new(
-    ENV['AWS_ACCESS_KEY_ID'],
-    ENV['AWS_SECRET_ACCESS_KEY'],
-    ENV['AWS_SESSION_TOKEN']
+# frozen_string_literal: true
+
+# This file is used to configure the AWS SDK for Ruby using the AWS SDK for Ruby V3.
+if ENV['AWS_WEB_IDENTITY_TOKEN_FILE'].present?
+  # Production environment using IRSA
+  role_credentials = Aws::AssumeRoleWebIdentityCredentials.new(
+    client: Aws::STS::Client.new(region: ENV['AWS_REGION']),
+    role_arn: ENV['AWS_ROLE_ARN'],
+    web_identity_token_file: ENV['AWS_WEB_IDENTITY_TOKEN_FILE'],
+    role_session_name: 'WebBackofficeCognitoSession'
   )
-})
+
+  Aws.config.update({
+                      region: ENV['AWS_REGION'],
+                      credentials: role_credentials
+                    })
+else
+  # Local development environment
+  Aws.config.update({
+                      region: ENV['AWS_REGION'],
+                      credentials: Aws::Credentials.new(
+                        ENV['AWS_ACCESS_KEY_ID'],
+                        ENV['AWS_SECRET_ACCESS_KEY'],
+                        ENV['AWS_SESSION_TOKEN']
+                      )
+                    })
+end
